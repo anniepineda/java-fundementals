de5e14debf0ad3638d203eb75aa9ae10cea48a02
anniepineda <anniepineda@outlook.com>
2020-01-08 13:22:04 -0800

    setting up lab02

--- basics/Main.java
+++ basics/Main.java
@@ -17,11 +17,35 @@ public class Main {
     }
     
 }
-    public static void flipHeads()
 
+/////FlipNHeads////
 
+    public static void flipNHeads(int n) {
+    //     int headsCounter = 0;
+    //     int flips = 0;
+    //     while (headsCounter != n){
+    //         flips++;
+    //         headsCounter = 0;
+    //         for (int i = 0; i < 20; i++){
+    //     int rand = (int)(Math.random() * .5);
+    //     //gives a random number between 0-1 and saves it in the variable rand
+    //     if (rand == 0){
+    //         System.out.println("heads");
+    //         headsCounter++;
+    //     
+    //     else if (rand == 1){
+    //         System.out.println("tails");
+    //     }
+    // }
+    // System.out.println("It took" + flips + "flips to flip" + n + "heads in a row.");
+    // }
+            int headsCounter = 0
+            while (headsCounter < n)
 
 
 
 }
 
+
+
+}
